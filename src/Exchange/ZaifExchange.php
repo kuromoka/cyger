f05a38f8b1a32ea9acd9b56ad2915a464212a2af
kuromoka <kuromokapb@gmail.com>
2017-12-13 23:33:27 +0900

    Add Zaif Exchange

---
+++ src/Exchange/ZaifExchange.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Cyptalt\Exchange;
+
+/**
+ * ZaifExchange Class
+ */
+class ZaifExchange extends BaseExchange
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getValidPairs($marketResults)
+    {
+        $validPairs = [];
+        foreach ($marketResults as $marketResult) {
+            if (isset($marketResult['currency_pair'])) {
+                $validPairs[] = $marketResult['currency_pair'];
+            }
+        }
+
+        return $validPairs;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getUrl($pairs)
+    {
+        foreach ($pairs as $key => $pair) {
+            if (!is_null($pair)) {
+                $pairs[$key] = $this->conf['baseUrl'] . $this->conf['tickerPath'] . $pair;
+            }
+        }
+
+        return $pairs;        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function parseResult($pairs, $jsonKey)
+    {
+        foreach ($pairs as $key => $pair) {
+            if (isset($pair[$this->conf[$jsonKey]])) {
+                $pairs[$key] = strval($pair[$this->conf[$jsonKey]]);
+            } else {
+                $pairs[$key] = null;
+            }
+        }
+        
+        return $pairs;        
+    }
+}
---
+++ tests/Exchange/ZaifExchangeTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Cyptalt\Exchange;
+
+use GuzzleHttp\Client;
+use Noodlehaus\Config;
+use Cyptalt\Exchange\ZaifExchange;
+
+/**
+ * ZaifExchangeTest Class
+ */
+class ZaifExchangeTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var array $conf config.json file content. */
+    private $conf;
+    /** @var array $testConf testconfig.json file content. */
+    private $testConf;
+
+    protected function setUp()
+    {
+        $confFile = __DIR__ . '/../../src/config.json';
+        $conf = new Config($confFile);
+        $this->conf = $conf->all();
+
+        $testConfFile = __DIR__ . '/../testconfig.json';
+        $testConf = new Config($testConfFile);
+        $this->testConf = $testConf->all();
+    }
+
+    public function testGetValidPairsWithNoEmptyMarketResults()
+    {
+        $expected = [
+            'btc_jpy',
+            'eth_btc',
+            'mona_btc',
+        ];
+
+        $client = new Client(['http_errors' => false]);
+        $exchange = new ZaifExchange($this->conf['Zaif'], $client);
+        $marketResults = [
+            [
+                'currency_pair' => 'btc_jpy',
+            ],
+            [
+                'currency_pair' => 'eth_btc',
+            ],
+            [
+                'currency_pair' => 'mona_btc',
+            ],
+        ];
+        $actual = $exchange->getValidPairs($marketResults);
+        
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetValidPairsWithEmptyMarketResults()
+    {
+        $expected = [];
+
+        $client = new Client(['http_errors' => false]);
+        $exchange = new ZaifExchange($this->conf['Zaif'], $client);
+        $marketResults = [];
+        $actual = $exchange->getValidPairs($marketResults);
+        
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetUrl()
+    {
+        $expected = [
+            'BTC_JPY' => $this->conf['Zaif']['baseUrl'] . $this->conf['Zaif']['tickerPath'] . 'btc_jpy',            
+        ];
+
+        $client = new Client(['http_errors' => false]);
+        $exchange = new ZaifExchange($this->conf['Zaif'], $client);
+        $pairs = [
+            'BTC_JPY' => 'btc_jpy',            
+        ];
+        $actual = $exchange->getUrl($pairs);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testParseResultWithValidJsonKey()
+    {
+        $expected = [
+            'btc_jpy' => '1000000',
+            'eth_btc' => '0.05',
+            'mona_btc' => '0.0001',
+        ];
+
+        $client = new Client(['http_errors' => false]);
+        $exchange = new ZaifExchange($this->conf['Zaif'], $client);
+        $pairs = [
+            'btc_jpy' => [
+                $this->conf['Zaif']['lastKey'] => 1000000,
+            ],
+            'eth_btc' => [
+                $this->conf['Zaif']['lastKey'] => 0.05,
+            ],
+            'mona_btc' => [
+                $this->conf['Zaif']['lastKey'] => 0.0001,
+            ],
+        ];
+        $actual = $exchange->parseResult($pairs, 'lastKey');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testParseResultWithInvalidJsonKey()
+    {
+        $expected = [
+            'btc_jpy' => null,
+            'eth_btc' => null,
+            'mona_btc' => null,
+        ];
+
+        $client = new Client(['http_errors' => false]);
+        $exchange = new ZaifExchange($this->conf['Zaif'], $client);
+        $pairs = [
+            'btc_jpy' => [
+                'lastPrice' => 1000000,
+            ],
+            'eth_btc' => [
+                'lastPrice' => 0.05,
+            ],
+            'mona_btc' => [
+                'lastPrice' => 0.0001,
+            ],
+        ];
+        $actual = $exchange->parseResult($pairs, 'lastKey');
+
+        $this->assertEquals($expected, $actual);
+    }
+}
